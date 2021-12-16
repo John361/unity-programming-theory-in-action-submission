d2acc5ece72446b1c484c186be1113a1205adc36
John H <hxjwork1@gmail.com>
2021-12-16 15:11:53 +0400

    Add method for Units management in GameDataManager

--- Assets/Scripts/GameDataManager.cs
+++ Assets/Scripts/GameDataManager.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 public class GameDataManager : MonoBehaviour
 {
-    // Public static attributes
+    // Public attributes
     public static GameDataManager Instance { get; private set; }
+    private readonly List<Unit> units = new List<Unit>();
 
     // Public attributes
-    public List<Unit> units = new List<Unit>();
     public bool isGameOver = false;
 
     // Private methods
@@ -21,5 +21,19 @@ public class GameDataManager : MonoBehaviour
 
         GameDataManager.Instance = this;
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Public methods
+    public Unit GetUnit(string name)
+    {
+        return this.units.Find(u => u.Name == name);
+    }
+
+    public void AddUnit(Unit unit)
+    {
+        if (this.GetUnit(unit.Name) == null)
+        {
+            this.units.Add(unit);
+        }
     }
 }

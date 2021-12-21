8e3af39dfe769fc98632a86120c0247eac3c84ec
John H <hxjwork1@gmail.com>
2021-12-21 13:54:44 +0400

    Use NavMesh agent on WorkerPlayer for another kind of walking

---
+++ Assets/Scripts/Workers/WorkerPlayer.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class WorkerPlayer : Worker
+{
+    // Private/Protected attributes
+    private NavMeshAgent navMeshAgent;
+    private List<Vector3> decorationPositions = new List<Vector3>();
+    private Vector3 currentPosition;
+
+    // Private/Protected methods
+    protected override void Start()
+    {
+        this.navMeshAgent = GetComponent<NavMeshAgent>();
+        this.GetDecorationPositions();
+
+        this.walkDuration = 5.0f;
+        this.coroutineWaitTime = 5.0f;
+
+        base.Start();
+    }
+
+    private void GetDecorationPositions()
+    {
+        GameObject[] decorations = GameObject.FindGameObjectsWithTag("Decoration");
+
+        foreach (GameObject decoration in decorations)
+        {
+            Vector3 position = decoration.transform.position;
+            this.decorationPositions.Add(position);
+        }
+    }
+
+    // Overrided methods
+    protected override IEnumerator Walk()
+    {
+        int index;
+        Vector3 randomDecorationPosition;
+        this.navMeshAgent.isStopped = true;
+
+        do
+        {
+            index = Random.Range(0, this.decorationPositions.Count);
+            randomDecorationPosition = this.decorationPositions[index];
+        }
+        while (randomDecorationPosition == this.currentPosition);
+
+        this.currentPosition = randomDecorationPosition;
+        this.navMeshAgent.SetDestination(randomDecorationPosition);
+        this.navMeshAgent.isStopped = false;
+
+        yield return null;
+    }
+}
